076aae700f5036267f0bfe6336a7bb45528bbb52
Artin Zarei <artin.zareie1383@gmail.com>
2025-02-05 10:03:06 +0330

    Add zeroconf

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/artinZareie/iori-sync
 
 go 1.23.4
+
+require (
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/grandcat/zeroconf v1.0.0 // indirect
+	github.com/miekg/dns v1.1.63 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/mod v0.23.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/tools v0.29.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"github.com/grandcat/zeroconf"
+)
+
+func serve(port int, password string) {
+	if password == "" {
+		fmt.Println("Error: password is required")
+		os.Exit(1)
+	}
+
+	server, err := zeroconf.Register("IoriSyncServer", "_http._tcp", "local.", port, []string{"txtv=0", "lo=1", "la=2"}, nil)
+
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		os.Exit(1)
+	}
+
+	defer server.Shutdown()
+
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, world!")
+	}))
+
+	fmt.Printf("Starting server on port %d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
 
 func main() {
-	fmt.Println("Welcome to Iori-Sync program.")
+	serveCmd := flag.NewFlagSet("serve", flag.ExitOnError)
+
+	port := serveCmd.Int("port", 8080, "Port to run the server on")
+	password := serveCmd.String("password", "", "Password for authentication (required)")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Please use -h to see available commands.")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "serve":
+		serveCmd.Parse(os.Args[2:])
+		serve(*port, *password)
+	default:
+		fmt.Printf("Unknown command: %s\n", os.Args[1])
+		os.Exit(1)
+	}
 }
